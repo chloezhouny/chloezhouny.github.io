9833e7949d886c917b70c89bfcbb506e0a8145f8
Chloe <chloezhouny@gmail.com>
2019-09-01 21:31:30 -0700

    add canvas particles to react

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React, { Component }from 'react';
+import Canvas from '../components/Canvas';
+
+
+
+class Home extends Component {
+
+ state = {
+
+ }
+
+  componentDidMount() {
+
+  }
+
+
+      render() {
+      	return (
+
+      		<Canvas/>
+
+
+
+      	);
+
+	}
+}
+
+
+
+export default Home;
