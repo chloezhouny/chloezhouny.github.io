d3332138ecf3585cf06b0f70d4f71cff7a646698
Chloe <chloezhouny@gmail.com>
2019-09-13 10:33:12 -0700

    nav bar responsiveness

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -103,9 +103,6 @@ render ()
 
 <nav ref='nav' className="sticky navbar navbar-expand-lg navbar-light bg-light">
   <a className="navbar-brand" href="#body" onClick={this.changeOpacity1}>cz</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav" >
       <li className="nav-item">
